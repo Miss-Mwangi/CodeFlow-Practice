983b63ae777db74a4926823f1b1347dd926ef32a
Rita Wanjiku <mwangirita@gmail.com>
2025-01-10 16:05:41 +0300

    Basics of Control Flow

---
+++ Control_Flow_and_Loops/Basics_of_Control_Flow.py
@@ -0,0 +1,10 @@
+number = int(input("Enter your number:  ")) # Get user input as an integer
+
+if number > 0:  # Check if the number is positive
+    print("Positive")
+
+elif number < 0:  # Check if the number is negative
+    print("Negative")
+
+else: # If neither positive nor negative, it must be zero
+    print("Zero")
